accb2775efa091142b89dbb4524329e71cd47597
GongYunHaoyyy <1501270490@qq.com>
2018-03-30 21:04:24 +0800

    MPAndroidChart

--- app/src/main/java/com/gongyunhao/nowmeeting/Activity/MeetingDetailActivity.java
+++ app/src/main/java/com/gongyunhao/nowmeeting/Activity/MeetingDetailActivity.java
@@ -1,6 +1,7 @@
 package com.gongyunhao.nowmeeting.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,11 +9,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.request.RequestOptions;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.gongyunhao.nowmeeting.Adapter.UserRecyclerviewAdapter;
 import com.gongyunhao.nowmeeting.Base.BaseActivity;
 import com.gongyunhao.nowmeeting.R;
@@ -21,7 +34,7 @@ import com.gongyunhao.nowmeeting.bean.UserItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MeetingDetailActivity extends BaseActivity {
+public class MeetingDetailActivity extends BaseActivity implements View.OnClickListener{
     private ImageView imageView_meeting_detail,imageView_qr_code;
     private int position;
     private String name,date,city;
@@ -29,6 +42,7 @@ public class MeetingDetailActivity extends BaseActivity {
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private RecyclerView recycler_rough;
     private UserRecyclerviewAdapter userRecyclerviewAdapter;
+    private RelativeLayout relativeLayout;
     private List<UserItem> userItems=new ArrayList<>(  );
     private TextView tv_detail_meeting_place,tv_detail_meeting_date;
     private String QR_CODE_CONTENT="Extra_Qr_Content";
@@ -57,17 +71,8 @@ public class MeetingDetailActivity extends BaseActivity {
             userItems.add(userItem2);
         }
 
-        imageView_qr_code.setOnClickListener( new View.OnClickListener( ) {
-            @Override
-            public void onClick(View view) {
-                /**
-                 * 在这里放会议的识别信息进去
-                 */
-                Intent intent=new Intent( MeetingDetailActivity.this,QrCodeActivity.class );
-                intent.putExtra( QR_CODE_CONTENT,"棒棒小糖测试,中文测试试用\nQR Code Content" );
-                startActivity( intent );
-            }
-        } );
+        imageView_qr_code.setOnClickListener( this );
+        relativeLayout.setOnClickListener( this );
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager( MeetingDetailActivity.this,LinearLayoutManager.HORIZONTAL,false );
         recycler_rough.setLayoutManager(linearLayoutManager);
@@ -96,6 +101,7 @@ public class MeetingDetailActivity extends BaseActivity {
         tv_detail_meeting_place.setText( city );
         imageView_qr_code=findViewById( R.id.iv_meeting_detail_qr_code );
         tv_detail_meeting_date.setText( date );
+        relativeLayout=findViewById( R.id.relativate_vote );
         getWindow().addFlags( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
     }
 
@@ -110,5 +116,23 @@ public class MeetingDetailActivity extends BaseActivity {
         pictureID=getIntent(  ).getIntExtra( "Extra_meeting_picture", 0 );
         city=getIntent(  ).getStringExtra( "Extra_meeting_city" );
         date=getIntent(  ).getStringExtra( "Extra_meeting_date" );
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick( v );
+        switch (v.getId()){
+            case R.id.iv_meeting_detail_qr_code:
+                /**
+                 * 在这里放会议的识别信息进去
+                 */
+                Intent intent=new Intent( MeetingDetailActivity.this,QrCodeActivity.class );
+                intent.putExtra( QR_CODE_CONTENT,"棒棒小糖测试,中文测试试用\nQR Code Content" );
+                startActivity( intent );
+                break;
+            case R.id.relativate_vote:
+                startIntent( PieChartActivity.class );
+                break;
+        }
     }
 }
