e1f8aafe1ee8c3b27cb0cd2b8143948e8f1e752f
GongYunHaoyyy <1501270490@qq.com>
2018-03-30 21:29:12 +0800

    meeting item can not click

--- app/src/main/java/com/gongyunhao/nowmeeting/Activity/ScanActivity.java
+++ app/src/main/java/com/gongyunhao/nowmeeting/Activity/ScanActivity.java
@@ -3,7 +3,6 @@ package com.gongyunhao.nowmeeting.Activity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.Toast;
